dfdc67d9e43f88c4e74b1d63e4497857d146f762
Vijay Reddy <vijayreddy.vanga8@gmail.com>
2024-01-09 11:29:01 +0530

    Created Products page.

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 let mybutton = document.getElementById("btn-back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 300px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
