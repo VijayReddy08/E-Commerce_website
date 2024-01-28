a8c04f1511eaea3b175083274d108dc8a0df2c11
Vijay Reddy <vijayreddy.vanga8@gmail.com>
2024-01-28 23:10:32 +0530

    Added Form Validation.

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+// Scroll to top btn
 let mybutton = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 200px from the top of the document, show the button
@@ -22,3 +23,28 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// Form validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
