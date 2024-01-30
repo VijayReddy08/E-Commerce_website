8794cf2485ac541c01e45a2e7a1beb8abf4322d6
Vijay Reddy <vijayreddy.vanga8@gmail.com>
2024-01-30 14:54:29 +0530

    Used JQuery to dynamically update nav bar.

---
+++ jquery/customJQuery.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  // Selects all <a> elements in the menu
+  var navLinks = $("nav ul li a");
+
+  // Iterates through each link
+  navLinks.each(function () {
+    // Checks if the link's href matches the current page's pathname
+    if (window.location.pathname.endsWith($(this).attr("href"))) {
+      // Adds the 'active' class to the matching link
+      $(this).parents("li").addClass("active");
+    } else {
+      // Removes the 'active' class from non-matching links
+      $(this).parents("li").removeClass("active");
+    }
+  });
+});
