43f036467cfcab978dd913c92c856a4ac094d3ad
Vijay Reddy <vijayreddy.vanga8@gmail.com>
2024-01-31 19:02:46 +0530

    Renamed a folder and some changes in custom JQuery.

--- jquery/customJQuery.js
+++ jquery/customJQuery.js
@@ -8,6 +8,9 @@ $(document).ready(function () {
     if (window.location.pathname.endsWith($(this).attr("href"))) {
       // Adds the 'active' class to the matching link
       $(this).parents("li").addClass("active");
+    } else if (window.location.pathname.endsWith("/")) {
+      // Adds the 'active' class to the home page
+      $("#home").addClass("active");
     } else {
       // Removes the 'active' class from non-matching links
       $(this).parents("li").removeClass("active");
